4e1274a438c9b6376644715d15d2ff228d883320
lilsongbirdhomes-creator <lilsongbirdhomes@gmail.com>
2026-03-01 12:11:20 -0600

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const path = require('path');
 const Database = require('better-sqlite3');
 const bcrypt = require('bcrypt');
 
-// Initialize database
+// Initialize database 
 const dbPath = process.env.DATABASE_PATH || '/app/database/scheduler.db';
 const db = new Database(dbPath);
 db.pragma('journal_mode = WAL');
